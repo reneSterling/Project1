d20d507723befa13ac0dbdcb464aa47552760eab
reneSterling <rene.boat23@gmail.com>
2025-07-10 21:07:53 +0100

    Vite and React app on Netlify

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1> Vite and React combination deployed using Netlify.</h1>
+        <h1> This web app uses Vite and React to display a message. We deployed the web app using Netlify.</h1>
+        
+
+      </div>
+     
+    </>
+  )
+}
+
+export default App
